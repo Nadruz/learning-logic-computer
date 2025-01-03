03f064a95c8650c44e7deb3dd5c4e57e803d8261
Juliana Nadruz <101072515+Nadruz@users.noreply.github.com>
2025-01-03 20:29:13 -0300

    Create EstudandoSwitchCase.js

---
+++ EstudandoSwitchCase.js
@@ -0,0 +1,24 @@
+let classificacao = "A"
+let pontos = 90
+
+switch (classificacao) {
+    case "A":
+        if (pontos >= 90) {
+         console.log("Conseguiu nota máxima!");    
+        } else {
+            console.log ("Quase alcançou A!");
+        }
+    break;    
+    
+    case "B":
+        if (pontos >=80) {
+            console.log("Alcançou nota média!");
+        } else {
+            console.log ("Quase alcançou B!");
+        }
+    break;
+    
+    case "C":
+        console.log("Desclassificado :(");
+    break;
+}
