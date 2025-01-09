dd025f02da7dfbb97dd41da92fcad3c69b91ea23
Nadruz <juliananadruz@gmail.com>
2025-01-09 20:19:10 -0300

    Aprofundamento de funcoes!

--- EstudandoFuncao/Aprendendo-TestandoFuncoes.js
+++ EstudandoFuncao/Aprendendo-TestandoFuncoes.js
@@ -29,3 +29,33 @@ function test(user){
     console.log(nome);
 }
 test(nome);
+
+//função é uma cidadã de primeira classe
+//função anônima -> Atribui uma função a uma variável
+let ola = function(nome) {
+    console.log(nome);
+}
+//ola("Ana Paula");
+
+//função a uma propriedade de um objeto | Declara um objeto literal, tendo a propriedade "exibir" passa a ser uma função
+let mensagem = {
+    exibir: function(msg) {
+        console.log(msg);
+    }
+}
+mensagem.exibir("olá")
+
+//função que é passada como argumento para outra função
+function exibirSaudacao(nome, func) {
+    func(nome);
+}
+exibirSaudacao("Ana Paula", ola);
+
+//function exibirSaudacao() {
+//     return function(nome, func) {
+//        func(nome);
+//    }
+//}
+//exibirSaudacao(("Ana Paula", ola));
+
+//callback -> Quando passamos uma função para dentro de outra função que vai ser executada posteriormente (algo que é retornado)
