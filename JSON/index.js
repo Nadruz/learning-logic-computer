5ff53116a6ae8b7913f6d2555c03b7fd5f105091
Nadruz <juliananadruz@gmail.com>
2025-01-16 17:43:32 -0300

    Aprendendo JSON!

---
+++ JSON/index.js
@@ -0,0 +1,25 @@
+//JASON JavaScript Object Notation
+// chave e valor com objetivo de transferir dados
+let invoice = {
+    name: "Juliana",
+    age: 20,
+    products: {
+        0: ["Mouse Logitech", 450.90],
+        1: ["Teclado Mecânico", 160.50],
+        2: ["Monitor 165hz", 880.90]
+    }
+}
+
+generateInvoice(invoice);
+
+function generateInvoice(invoice){
+    console.log(`O comprador é ${invoice.name}`);
+    console.log(`A idade é ${invoice.age}`);
+    console.log(`---------------`);
+
+//desestruturação
+    for(let index in invoice.products){
+        let [productName, productPrice] = invoice.products[index]
+        console.log(`O produto é ${productName} no valor de ${productPrice}`);
+    }
+}
